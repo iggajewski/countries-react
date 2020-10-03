a7a2259345df18b23fa556cc1a0a1a724e99c138
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-10-03 11:07:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import countriesAll from "./countriesAll.json";
 import './App.css';
 
-function App() {
+function CountryElem(country) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="country">
+      <img src={country.flag} alt="flag"/>
+      <h2>{country.name}</h2>
+      <div className="countryData">
+        <p><b>Population:  </b>{country.population}</p>
+        <p><b>Region:  </b>{country.region}</p>
+        <p><b>Capital:  </b>{country.capital}</p>
+      </div>
     </div>
   );
 }
 
+function CountryList(props) {
+  return (
+    <div className="countryList">
+      {props.countries.map(CountryElem)}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div id="page">
+      <CountryList countries={countriesAll}/>
+    </div>
+  );
+}
 export default App;
