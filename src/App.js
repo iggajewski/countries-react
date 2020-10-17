84d5e1c99cd8c029fe3d8bad7e8971f32added9d
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-10-17 05:02:47 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -13,15 +13,13 @@ function Search(props) {
   useEffect(() => props.search(searchInput), [searchInput]);
 
   return (
-    <div id="search">
-      <input 
-        type="search"
-        id="searchBar"
-        value={searchInput}
-        onInput={handleSearchInput}
-        placeholder="Search"
-      />
-    </div>
+    <input 
+      type="search"
+      id="searchBar"
+      value={searchInput}
+      onInput={handleSearchInput}
+      placeholder="Search"
+    />
   );
 }
 
@@ -53,7 +51,7 @@ function CountryElem(props) {
       <img src={props.country.flag} alt="flag"/>
       <h2>{props.country.name}</h2>
       <div className="countryData">
-        <p><b>Population:  </b>{props.country.population}</p>
+        <p><b>Population:  </b>{props.country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
         <p><b>Region:  </b>{props.country.region}</p>
         <p><b>Capital:  </b>{props.country.capital}</p>
       </div>
@@ -62,56 +60,6 @@ function CountryElem(props) {
 }
 
 function CountryList(props) {
-  return (
-    <div className="countryList"> 
-      {props.countries.map(country => <CountryElem country={country} onSelect={country => props.select(country)}/>)}
-    </div>
-  );
-}
-
-function IndividualCountry(props) {
-  let borderCountries = [];
-  for(let i = 0; i < props.country.borders.length; ++i){
-    for(let j = 0; j < countriesAll.length; ++j) {
-      if(props.country.borders[i] === countriesAll[j].alpha3Code) {
-        borderCountries.push(countriesAll[j]);
-        break;
-      }
-    }
-  }
-    
-  return (
-    <div className="individualCountryElem">
-      <button onClick={() => props.changeSelection(null)}>Back</button>
-      <div className="individualCountry">
-      <img src={props.country.flag} alt="flag"/>
-      <div className="details">
-        <h2>{props.country.name}</h2>
-        <div className="IndCntrData">
-          <div className="IndCntrData1">
-            <p><b>Native Name:  </b>{props.country.nativeName}</p>
-            <p><b>Population:  </b>{props.country.population}</p>
-            <p><b>Region:  </b>{props.country.region}</p>
-            <p><b>Subregion:  </b>{props.country.subregion}</p>
-            <p><b>Capital:  </b>{props.country.capital}</p>
-          </div>
-          <div className="IndCntrData2">
-            <p><b>Top Level Domain:  </b>{props.country.topLevelDomain[0]}</p>
-            <p><b>Currencies:  </b>{props.country.currencies.map((cur) => cur.name + ", ")}</p>
-            <p><b>Languages:  </b>{props.country.languages.map((lang) => lang.name + ", ")}</p>
-          </div>
-        </div>
-          <ul className="borderCountries">
-            {borderCountries.map(neighbor => <li><button onClick={() => props.changeSelection(neighbor)}>{neighbor.name}</button></li>)}
-          </ul>
-        </div> 
-      </div>
-    </div>
-    
-  );
-}
-
-function App() {
   let [listOfCountries, setListOfCountries] = useState(countriesAll);
 
   function searchCountries(keyword) {
@@ -145,27 +93,90 @@ function App() {
     setListOfCountries(newCountryList);
   }
 
+
+  return (
+    <div className="countryListElem">
+      <div id="navigationBar">
+        <Search search={searchCountries}/>
+        <SelectRegion select={filterCountries}/>
+      </div>
+      <div className="countryList"> 
+        {listOfCountries.map(country => <CountryElem country={country} onSelect={country => props.select(country)}/>)}
+      </div>
+    </div>
+    
+  );
+}
+
+function IndividualCountry(props) {
+  let borderCountries = [];
+  for(let i = 0; i < props.country.borders.length; ++i){
+    for(let j = 0; j < countriesAll.length; ++j) {
+      if(props.country.borders[i] === countriesAll[j].alpha3Code) {
+        borderCountries.push(countriesAll[j]);
+        break;
+      }
+    }
+  }
+  console.log(borderCountries)
+    
+  return (
+    <div className="individualCountryElem">
+      <button className="backButton" 
+              onClick={() => props.changeSelection(null)}
+        >← Back
+      </button>
+
+      <div className="individualCountry">
+        <img src={props.country.flag} alt="flag"/>
+
+        <div className="details">
+          <h2>{props.country.name}</h2>
+
+          <div className="IndCntrData">
+            <div className="IndCntrData1">
+              <p><b>Native Name:  </b>{props.country.nativeName}</p>
+              <p><b>Population:  </b>{props.country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+              <p><b>Region:  </b>{props.country.region}</p>
+              <p><b>Subregion:  </b>{props.country.subregion}</p>
+              <p><b>Capital:  </b>{props.country.capital}</p>
+            </div>
+
+            <div className="IndCntrData2">
+              <p><b>Top Level Domain:  </b>{props.country.topLevelDomain[0]}</p>
+              <p><b>Currencies:  </b>
+                {props.country.currencies.map((cur) => cur.name + (cur === props.country.currencies[props.country.currencies.length - 1] ? "" : ", "))}</p>
+              <p><b>Languages:  </b>
+                {props.country.languages.map((lang) => lang.name + (lang === props.country.languages[props.country.languages.length - 1] ? "" : ", "))}</p>
+            </div>
+          </div>
+
+          <ul className="borderCountries">
+            <span><b>Border Countries: </b></span>
+            {(borderCountries.length) ? "" : <span><b>None</b></span>}
+            {borderCountries.map(neighbor => <li><button className="neighborButton" onClick={() => props.changeSelection(neighbor)}>{neighbor.name}</button></li>)}
+          </ul>
+        </div> 
+      </div>
+    </div>
+    
+  );
+}
+
+function App() {
   let [selectedCountry, setSelectedCountry] = useState(null);
 
-  if(selectedCountry) {
-    return (
-      <div id="page">
-        <div id="topbar"></div>
+  return (
+    <div id="page">
+      <div id="topbar">
+        <h1>Where in the world?</h1>
+      </div>
+      {selectedCountry ? 
         <IndividualCountry country={selectedCountry} changeSelection={country => setSelectedCountry(country)}/>
-      </div>
-    );
-  }
-  else {
-    return(
-      <div id="page">
-        <div id="topbar">
-          <Search search={searchCountries}/>
-          <SelectRegion select={filterCountries}/>
-        </div>
-        <CountryList countries={listOfCountries} select={country => setSelectedCountry(country)}/>
-      </div>
-    );
-  }
+      : <CountryList select={country => setSelectedCountry(country)}/>
+      }   
+    </div>
+  );
 }
 
 export default App;
@@ -173,12 +184,6 @@ export default App;
 /*
 
 TODO:
-
-Level 4 challenge (PARTIALLY COMPLETED)
-
-    Allow users to click on a country and view that country's full details, as shown in the frontendmentor challenge.
-    Make sure to allow clicking on bordering countries to visit these neighbours
-    Don't forget the "back" button
 
 Level 5 challenge
 
